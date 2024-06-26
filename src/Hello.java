cbfe2662357264dc85d9eeea2e12208606ffaa31
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-06-26 13:36:04 +0530

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,25 @@
+
+public class Hello {
+    //this is block
+    //psvm sourtcut
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+//        int age=26;
+        boolean isPassed = true;
+//        long countAge = age;
+//        int secoundMarks = 23;  // camelCase
+        // Some special keywords is not allow in name convention
+        // byte mark = 1400; this is not allowed
+        byte mark = 13;
+        short studentCount = 1234;
+        float pi = 3.15f;
+        double secoundPi = 3.14324324564;
+
+        char myLetter = 'A';
+        System.out.println(myLetter);
+//        Date type implicit conversion
+          int age = 150;
+          byte newAge = (byte)age;
+        System.out.println(newAge);
+    }
+}
